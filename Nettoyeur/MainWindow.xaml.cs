544878f022c29cc670075c731ff3feb801b5148b
A_Rijenth <rijenth93100@laposte.net>
2022-10-02 12:19:58 +0200

    logic improvement

--- Nettoyeur/MainWindow.xaml.cs
+++ Nettoyeur/MainWindow.xaml.cs
@@ -13,27 +13,32 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml.Serialization;
 
-namespace Nettoyeur
+namespace CleanMyComputer
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
-        private string _SoftwareName = "MonLogiciel";
-        string version = "0.0.0";
+        private string _softwareName = "Clean My Computer";
+        private string _version = "0.0.1";
+        private long _overallCleanedSize;
+        private long _hasBeenCleaned;
+        private bool _analyseCompleted;
         public DirectoryInfo WinTemp;
         public DirectoryInfo AppTemp;
         public MainWindow()
         {
             InitializeComponent();
             SetAppName();
+            Version();
             WinTemp = new DirectoryInfo(@"C:\Windows\Temp");
             AppTemp = new DirectoryInfo(System.IO.Path.GetTempPath());
             CheckActu();
@@ -48,13 +53,17 @@ namespace Nettoyeur
             try
             {
                 string url = "http://localhost:81/CleanMyComputer/informationPrimaire.txt";
+
                 using (WebClient client = new WebClient())
                 {
                     string information = client.DownloadString(url);
-                    if (information != String.Empty)
+
+                    if (information != string.Empty)
                     {
                         actu.Content = information;
+
                         actu.Visibility = Visibility.Visible;
+
                         bandeau.Visibility = Visibility.Visible;
                     }
                 };
@@ -65,6 +74,10 @@ namespace Nettoyeur
             
         }
 
+        public void Version()
+        {
+            softwareVersion.Content = "Version du logiciel : " + _version;
+        }
         
         public void CheckVersion()
         {
@@ -75,17 +88,30 @@ namespace Nettoyeur
                 try
                 {
                     string v = client.DownloadString(url);
-                    if (version != v)
+                    if (_version != v)
                     {
-                        MessageBox.Show("Une nouvelle version est disponible !", "Mise à jour", MessageBoxButton.OK, MessageBoxImage.Information);
+                        MessageBox.Show(
+                            "Une nouvelle version est disponible !", 
+                            "Mise à jour", MessageBoxButton.OK, 
+                            MessageBoxImage.Information
+                            );
+
+                        _version = v;
+
+                        Version();
                     }
                     else
                     {
-                        MessageBox.Show("Votre logiciel est à jour", "Mise à jour", MessageBoxButton.OK, MessageBoxImage.Information);
+                        MessageBox.Show(
+                            "Votre logiciel est à jour", 
+                            "Mise à jour", 
+                            MessageBoxButton.OK, 
+                            MessageBoxImage.Information
+                            );
                     }
                 } catch 
                 {
-                    MessageBox.Show("Connection au serveur impossible.");
+                    MessageBox.Show("Connexion au serveur impossible.");
                 }
                 
             };
@@ -107,7 +133,9 @@ namespace Nettoyeur
                 try
                 {
                     fichier.Delete();
+
                     Console.WriteLine(fichier.FullName);
+
                     //totalRemovedFiles++; Si besoin statistique
                 }
                 catch (Exception error)
@@ -121,7 +149,9 @@ namespace Nettoyeur
                 try
                 {
                     repertoire.Delete(true); // true car on supprime aussi tout ce qui a l'intérieur d'un dossier
+
                     Console.WriteLine(repertoire.FullName);
+
                     //totalRemovedDirectories++; Si besoin statistique
                 }
                 catch (Exception error)
@@ -131,36 +161,88 @@ namespace Nettoyeur
             }
         }
 
-        public string SoftwareTitle
-        {
-            get { return _SoftwareName; }
-            set { _SoftwareName = value; }
-
-        }
-
         public void SetAppName()
         {
-            appName.Title = SoftwareTitle;
+            appName.Title = _softwareName;
         }
 
         private void ButtonAnalyse(object sender, RoutedEventArgs e)
         {
+            btnCleanMessage.Content = "Nettoyage";
+
             AnalyseFolders();
+
+            _analyseCompleted = true;
         }
 
-        /// <summary>
-        /// Nettoie le presse papier, le dossier WinTemp et AppTemp
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void ButtonNettoyage(object sender, RoutedEventArgs e)
         {
-            Clipboard.Clear();
+            if (_analyseCompleted)
+            {
+                Nettoyage();
+
+                MessageBox.Show(
+                    "Vous avez effectivement nettoyé " + _hasBeenCleaned + " MB",
+                    caption: _softwareName,
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information
+                );
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Il faut lancer une analyse avant de pouvoir utiliser la fonction de nettoyage.",
+                    caption: _softwareName,
+                    MessageBoxButton.OK,
+                    MessageBoxImage.Information
+                );
+            }
             
+        }
+
+
+        private void ButtonHistorique(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists("historique.txt"))
+            {
+                string data = File.ReadAllText("historique.txt");
+
+                long totalCleanedSize = Convert.ToInt64(data.Substring(data.IndexOf('/') + 1));
+
+                MessageBox.Show(
+                    "Depuis que vous utilisez ce logiciel, vous avez retiré " + totalCleanedSize + " MB de fichiers et dossiers indésirable.", 
+                    caption: _softwareName, 
+                    MessageBoxButton.OK, 
+                    MessageBoxImage.Information
+                    );
+            }
+            else
+            {
+                MessageBox.Show(
+                    "Aucun historique disponible. Il faut lancer une première analyse afin d'avoir un historique.",
+                    caption:_softwareName, 
+                    MessageBoxButton.OK, 
+                    MessageBoxImage.Information
+                    );
+            }
+        }
+
+        private void Nettoyage()
+        {
+            _hasBeenCleaned = 0;
+
+            Clipboard.Clear();
+
             try
             {
+                long beforeCleaning = DirectorySize(WinTemp) / 1000000;
+
                 ClearTempData(WinTemp);
-            } 
+
+                long afterCleaning = DirectorySize(WinTemp) / 1000000;
+
+                _hasBeenCleaned += beforeCleaning - afterCleaning;
+            }
             catch (Exception error)
             {
                 Console.WriteLine("Erreur : " + error.Message);
@@ -168,7 +250,14 @@ namespace Nettoyeur
 
             try
             {
+                long beforeCleaning = DirectorySize(AppTemp) / 1000000;
+
                 ClearTempData(AppTemp);
+
+                long afterCleaning = DirectorySize(AppTemp) / 1000000;
+
+
+                _hasBeenCleaned += beforeCleaning - afterCleaning;
             }
             catch (Exception error)
             {
@@ -176,31 +265,19 @@ namespace Nettoyeur
             }
 
             btnCleanMessage.Content = "Nettoyage terminé !";
+
             Titre.Content = "Le nettoyage a été effectué";
-            espace.Content = "0 Mb";
+
+            espace.Content = "0 MB";
+
+            UpdateHistorique();
         }
 
-
-        private void ButtonHistorique(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Fonction à développer", caption: "historique", MessageBoxButton.OK, MessageBoxImage.Information);
-        }
-
-        /// <summary>
-        /// Verifie la version du logiciel depuis un fichier texte sur le serveur
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void ButtonMaj(object sender, RoutedEventArgs e)
         {
             CheckVersion();
         }
 
-        /// <summary>
-        /// A l'appui du bouton, redirige l'utilisateur vers une page web
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
         private void ButtonMoreInfo(object sender, RoutedEventArgs e)
         {
             try
@@ -218,7 +295,6 @@ namespace Nettoyeur
 
         public void AnalyseFolders()
         {
-            Console.WriteLine("Début de l'analyse...");
             long totalSize = 0;
 
             // Donnée sensible, bonne pratique car privilège admin
@@ -226,6 +302,7 @@ namespace Nettoyeur
             {
                 // DirectorySize(WinTemp) renvoie une taille en octet, on divise afin d'avoir une valeur en Mb.
                 totalSize += DirectorySize(WinTemp) / 1000000;
+
                 totalSize += DirectorySize(AppTemp) / 1000000;
             }
             catch (Exception error)
@@ -235,27 +312,76 @@ namespace Nettoyeur
 
             // On met à jour les champs de l'application
             espace.Content = totalSize + " Mb à nettoyer";
+
             Titre.Content = "Analyse effectuée";
+
             date.Content = DateTime.UtcNow.ToString("dddd, dd MMMM yyyy");
-            SaveDate();
+
+            SaveDateAndEspace(totalSize);
+            
         }
        
-        public void SaveDate()
+        public void SaveDateAndEspace(long totalSize)
         {
-            string DateOfLastAnalyse = DateTime.UtcNow.ToString("dddd, dd MMMM yyyy");
-            File.WriteAllText("date.txt", DateOfLastAnalyse);   
+            GetOverallCleanedSize();
+
+            string Info = date.Content + ":" + totalSize + "/" + _overallCleanedSize;
+
+            File.WriteAllText("historique.txt", Info);
+
+            _hasBeenCleaned = 0;
+
         }
 
         public void GetDate()
         {
-            if (File.Exists("date.txt"))
+            if (File.Exists("historique.txt"))
             {
-                string LastAnalyseDate = File.ReadAllText("date.txt");
-                if (LastAnalyseDate != String.Empty)
+                string lastAnalyseDate = File.ReadAllText("historique.txt");
+                lastAnalyseDate = lastAnalyseDate.Split(':')[0];
+
+                if (lastAnalyseDate != String.Empty)
                 {
-                    date.Content = LastAnalyseDate;
+                    date.Content = lastAnalyseDate;
                 }
             }   
+        }
+
+        public void GetOverallCleanedSize()
+        {
+            if (File.Exists("historique.txt"))
+            {
+                string data = File.ReadAllText("historique.txt");
+
+                _overallCleanedSize = Convert.ToInt64(data.Substring(data.IndexOf('/') + 1));
+            }
+            else
+            {
+                _overallCleanedSize = 0;
+            }
+        }
+        public void UpdateHistorique()
+        {
+            if (File.Exists("historique.txt"))
+            {
+                string data = File.ReadAllText("historique.txt");
+
+                string dateOfAnalyse = data.Split(':')[0];
+
+                string totalSize = data.Split('/')[0];
+
+                totalSize = totalSize.Substring(totalSize.IndexOf(':') + 1);
+
+                long totalSizeToLong = Convert.ToInt64(totalSize); 
+
+                GetOverallCleanedSize();
+
+                _overallCleanedSize += _hasBeenCleaned;
+
+                string Info = dateOfAnalyse + ":" + totalSizeToLong + "/" + _overallCleanedSize;
+
+                File.WriteAllText("historique.txt", Info);
+            }
         }
     }
 }
